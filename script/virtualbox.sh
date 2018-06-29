1e3cdaf00297ced826d4f243518a3741244cb4a5
Olivier Berger <olivier.berger@telecom-sudparis.eu>
2018-06-29 14:33:14 +0200

    Trying recompiling VirtualBox extensions from development branch for F28 server

--- script/virtualbox.sh
+++ script/virtualbox.sh
@@ -4,13 +4,14 @@ SSH_USER=${SSH_USERNAME:-vagrant}
 SSH_USER_HOME=${SSH_USER_HOME:-/home/${SSH_USER}}
 
 if [[ $PACKER_BUILDER_TYPE =~ virtualbox ]]; then
+    VBOX_VERSION=$(cat $SSH_USER_HOME/.vbox_version)
+    echo "==> Currently running VirtualBox $VBOX_VERSION"
     echo "==> Installing VirtualBox guest additions"
     # Some of these packages should already have been installed in the kickstart
-    dnf -y install kernel-headers-"$(uname -r)" kernel-devel-"$(uname -r)" gcc make perl
+    dnf -y install kernel-headers-"$(uname -r)" kernel-devel-"$(uname -r)" gcc make perl dkms
     # Need to set the KERN_DIR in Fedora 26 through 28 or the VBox additions will not install
     /usr/libexec/system-python -mplatform | grep -qi "fedora-2[678]" && echo "Fedora 26, 27 or 28 detected" && \
-    KERN_DIR=/usr/src/kernels/"$(uname -r)" && export KERN_DIR
-    VBOX_VERSION=$(cat $SSH_USER_HOME/.vbox_version)
+    KERN_DIR=/lib/modules/"$(uname -r)"/build && export KERN_DIR
     mount -o loop $SSH_USER_HOME/VBoxGuestAdditions_$VBOX_VERSION.iso /mnt
     sh /mnt/VBoxLinuxAdditions.run --nox11
     umount /mnt
