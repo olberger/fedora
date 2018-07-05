505064c2563857df0f8a1ead3b336b07ed84ffd8
Olivier Berger <olivier.berger@telecom-sudparis.eu>
2018-07-05 09:30:04 +0200

    Require kernel >= 4.17.4, not yet in F 28 updates
    The vboxguest mouse emulation isn't working properly in 4.17.3 which is
    in Fedora 28 updates ATM, so we rely on a built kernel not yet released.
    
    This is probably going to change soon

--- script/update.sh
+++ script/update.sh
@@ -1,8 +1,32 @@
 #!/bin/bash -eux
+
+function ver { printf "%03d%03d%03d%03d" $(echo "$1" | tr '.' ' '); }
+
 if [[ $UPDATE  =~ true || $UPDATE =~ 1 || $UPDATE =~ yes ]]; then
     echo "==> Applying updates"
     dnf -y update
 
+    # Check if we're running Fedora 28
+    grep -qi "Fedora release 28" /etc/fedora-release 
+    if [ $? -eq 0 ]; then
+        # Check if kernel is lower than required 4.17.4
+        kernel_version=$(uname -r | sed 's/-.*//g')
+        # if we lag behind, install not yet released kernel from https://koji.fedoraproject.org/koji/buildinfo?buildID=1102770
+        if [ $(ver $kernel_version) -lt $(ver 4.17.4) ]; then
+            echo "Need to update kernel to 4.17.4 needed for VirtualBox"
+            mkdir kernel_update
+            cd kernel_update
+            packages="kernel-4.17.4-200.fc28.x86_64.rpm kernel-core-4.17.4-200.fc28.x86_64.rpm kernel-devel-4.17.4-200.fc28.x86_64.rpm kernel-headers-4.17.4-200.fc28.x86_64.rpm kernel-modules-4.17.4-200.fc28.x86_64.rpm kernel-modules-extra-4.17.4-200.fc28.x86_64.rpm"
+            for p in $packages
+            do
+                wget -nv https://kojipkgs.fedoraproject.org//packages/kernel/4.17.4/200.fc28/x86_64/$p
+            done
+            yum install -y $packages
+            cd ..
+            rm -fr kernel_update
+        fi
+
+    fi
     # reboot
     echo "Rebooting the machine..."
     reboot
